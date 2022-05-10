d5cb890391b7b8b49244ade2bc2104c46f04b5a4
MikeWongNZAKL <mike@LAPTOP-ANUOQFLF.localdomain>
2022-05-10 17:08:39 +1200

    basic whack a mole done - still need to fix some stuff

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,33 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+game = gameBoard.getElementsByTagName('td')
+
+function randomNumber() {
+  var box = Math.floor(Math.random() * (24 - 0 + 1)) + 0
+  return box
+}
+
+var number = randomNumber()
+var randomPlacement = game[number]
+
+var mole = document.createElement('img')
+mole.src = './mole.PNG'
+mole.id = 'mole'
+
+randomPlacement.appendChild(mole)
+
+var audio = new Audio()
+audio.src = './whack-audio.wav'
+
+function whackedMole() {
+  number = randomNumber()
+  randomPlacement = game[number]
+  randomPlacement.appendChild(mole)
+  audio.play()
+}
+
+mole.onclick = whackedMole
